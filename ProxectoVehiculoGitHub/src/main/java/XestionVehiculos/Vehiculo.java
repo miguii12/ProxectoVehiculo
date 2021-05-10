34947dd163c2a573fb671522520eca00c297f872
DAM1_Alu05 <DAM1_Alu05@equip077238.xunta.local>
2021-05-10 20:37:18 +0200

    Engadida clase Vehiculo

---
+++ ProxectoVehiculoGitHub/src/main/java/XestionVehiculos/Vehiculo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package XestionVehiculos;
+
+/**
+ *
+ * @author DAM1_Alu05
+ */
+public class Vehiculo {
+    
+}
